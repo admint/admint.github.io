f83302d18bc5fe02bd02377abae3e7ff8cda81e6
admint <mint.nobody@gmail.com>
2017-09-29 00:32:38 +0700

    test1

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '43.241.57.10',
+        'user': 'drycliniqu_pac',
+        'password': 'mint0326'
+    },
+	'database': 'drycliniqu_pac'
+};
